6ceba378fa6ed557607698c833011e46e24cec73
calvincho95 <calvincho95@naver.com>
2024-05-13 15:33:09 +0900

    weather-latitude, longitude

---
+++ src/components/WeatherIcon.jsx
@@ -0,0 +1,18 @@
+const WeatherIcon = () => {
+  const WeatherIcon = () => {
+    const [latitude, setLatitude] = useState();
+    const [longitude, setLongitude] = useState();
+
+    useEffect(() => {
+      navigator,
+        geolocation.getCurrentPosition((position) => {
+          setLatitude(position.coords, latitude);
+          setLongitude(position.coords.longitude);
+        });
+    }, []);
+
+    return <div>WeatherIcon</div>;
+  };
+};
+
+export default WeatherIcon;
