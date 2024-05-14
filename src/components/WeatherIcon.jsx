467e57736febf20b034e655c5b45fb1c73708b0d
calvincho95 <calvincho95@naver.com>
2024-05-14 10:38:11 +0900

    weathericon

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -1,11 +1,30 @@
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
-import { WiCloud } from "react-icons/wi";
-import { WiDaySunny } from "react-icons/wi";
-import { WiDayCloudy } from "react-icons/wi";
-import { WiCloudy } from "react-icons/wi";
+import {
+  WiCloud,
+  WiDaySunny,
+  WiDayCloudy,
+  WiCloudy,
+  WiRain,
+  WiLightning,
+  WiSnow,
+  WiFog,
+} from "react-icons/wi";
 
+const sunny = <WiDaySunny size={32} />;
+
+const wetherIcons = {
+  "01": <WiDaySunny size={32} />,
+  "02": <WiDayCloudy size={32} />,
+  "03": <WiCloud size={32} />,
+  "04": <WiCloudy size={32} />,
+  "09": <WiRain size={32} />,
+  10: <WiRain size={32} />,
+  11: <WiLightning size={32} />,
+  13: <WiSnow size={32} />,
+  50: <WiFog size={32} />,
+};
 const WeatherIcon = () => {
   const [latitude, setLatitude] = useState();
   const [longitude, setLongitude] = useState();
@@ -49,18 +68,7 @@ const WeatherIcon = () => {
 
   return (
     <div className="text-xs flex items-center">
-      {weatherData.weather[0].icon.substring(0, 2) === "01" && (
-        <WiDaySunny size={32} />
-      )}
-      {weatherData.weather[0].icon.substring(0, 2) === "02" && (
-        <WiDayCloudy size={32} />
-      )}
-      {weatherData.weather[0].icon.substring(0, 2) === "03" && (
-        <WiCloud size={32} />
-      )}
-      {weatherData.weather[0].icon.substring(0, 2) === "04" && (
-        <WiCloudy size={32} />
-      )}
+      {wetherIcons[weatherData.weather[0].icon.substring(0, 2)]}
       <div className="w-16">
         <div className="font-semibold">{weatherData.name}</div>
         <div>{weatherData.main.temp.toFixed(1)}℃</div>
